f9f64060043d9e04a797289479d0ca577eb0caaf
dat-nguyen-1 <dat.nguyen@stu.ocsb.ca>
2026-02-24 18:02:56 +0000

    shows temperature in kelvin

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,9 +1,19 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Dat Nguyen
+Created on: Feb 2026
+This program will display the current temperature on the Micro:bit.
 """
 
 from microbit import *
 
-display.scroll("Hello, World!")
+current_temperature_celsius: int
+current_temperature_kelvin: int
+
+display.clear()
+display.show(Image.HAPPY)
+
+while True:
+    if button_a.is_pressed():
+        current_temperature_celsius = temperature()
+        current_temperature_kelvin = round(current_temperature_celsius + 273.15)
+        display.scroll("The temperature is: " + str(current_temperature_kelvin) + " C.")
