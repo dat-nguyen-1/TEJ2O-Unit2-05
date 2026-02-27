722759a666ccbccab71922c6eecd4be418acbc48
dat-nguyen-1 <dat.nguyen@stu.ocsb.ca>
2026-02-27 17:57:55 +0000

    ad comments and remove variable declaration and type hints

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -6,14 +6,14 @@ This program will display the current temperature on the Micro:bit.
 
 from microbit import *
 
-current_temperature_celsius: int
-current_temperature_kelvin: int
-
+# Initialization
 display.clear()
 display.show(Image.HAPPY)
 
+# Main loop
 while True:
     if button_a.is_pressed():
         current_temperature_celsius = temperature()
         current_temperature_kelvin = round(current_temperature_celsius + 273.15)
+
         display.scroll("The temperature is: " + str(current_temperature_kelvin) + " C.")
