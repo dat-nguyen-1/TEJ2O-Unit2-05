db53ac579d01185753db6b1acf1b152e58a6c55d
dat-nguyen-1 <dat.nguyen@stu.ocsb.ca>
2026-03-05 13:18:23 -0500

    Implement temperature conversion to Kelvin
    Add functionality to display temperature in Kelvin.

--- main.ts
+++ main.ts
@@ -5,17 +5,21 @@
  * This program will display the current temperature in kelvin on the micro:bit.
 */
 
+// Initialize variables.
 let currentTemperatureCelsius: number
 let currentTemperatureKelvin: number
 
+// Initialize display.
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
+    // Calculate temperature in kelvin.
     currentTemperatureCelsius = input.temperature()
     currentTemperatureKelvin = currentTemperatureCelsius + 273.15
     currentTemperatureKelvin = Math.round(currentTemperatureKelvin)
 
+    // Ouput temperature in kelvin.
     basic.clearScreen()
     basic.showString('The temperature is: ' + currentTemperatureKelvin.toString() + ' K.')
 })
